647149d091963b0a2ac019c4fb5169eb10340498
Mohamedsaber45 <120626930+Mohamedsaber45@users.noreply.github.com>
2022-12-15 09:28:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_prime_number_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            for (int num = 5; num <= 100; num++)
+            {
+                int count = 0;
+                for (int p = 1; p < num; p++)
+                {
+
+                    if (num % p == 0)
+                    {
+                        count += 1;
+
+                    }
+                }
+                if (count < 2)
+                {
+                    Console.WriteLine(num);
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
